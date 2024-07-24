2f51a0af122eaa415066eefff9b96f5f6c031d4e
boxofzero <elnino.ftorres09@gmail.com>
2024-07-24 21:33:37 +0700

    some head tag and favicon

--- app.vue
+++ app.vue
@@ -4,3 +4,17 @@
 		<UNotifications />
 	</NuxtLayout>
 </template>
+
+<script setup>
+useHead({
+	title: 'Cassowary : Wuthering Waves Planner',
+	meta: [
+		{
+			name: 'description',
+			content: 'Cassowary : Wuthering Waves Planner',
+		},
+	],
+
+	link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+});
+</script>
