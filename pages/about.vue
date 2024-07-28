c6abf5c793165835c23f34b8ff9b5d22f97d2b7e
boxofzero <elnino.ftorres09@gmail.com>
2024-07-28 10:29:33 +0700

    update about pages

--- pages/about.vue
+++ pages/about.vue
@@ -4,34 +4,75 @@
 	</section>
 	<section>
 		<div>
-			<span>thanks to wutheringwaves.gg for all the data</span>
-			<br />
-			<span>random trivia</span>
-			<br />
-			<span>char_exp : credit_to_level = 1 : 0,35</span>
-			<br />
-			<span
-				>weapon_exp : credit_to_level = 1 : 0,4 (allegedly. CMIIW . The
-				calculation kinda funky)</span
-			>
+			<div>
+				thanks to seelie.me for the inspiration. you are the goat of gacha game
+				planner.
+			</div>
+			<div>thanks to wutheringwaves.gg for all the data</div>
+			<div>#####################################</div>
+			<div>random trivia</div>
+			<div>char_exp : credit_to_level = 1 : 0,35</div>
+			<div>
+				weapon_exp : credit_to_level = 1 : 0,4 (allegedly. CMIIW . The
+				calculation kinda funky)
+			</div>
+			<div>#####################################</div>
+			<div>
+				about echo levelling: (CMIIW , i may need to check more in-game)
+			</div>
+			<div>
+				to level 5-star echo from 1 to 25, you need around 29 Premium Sealed
+				Tube
+			</div>
+			<div>
+				at sol3 rank 6, each run at tacet field you can get at least 3 Premium
+				Sealed Tube and 3 Advanced Sealed Tube, which is valued as 1.2 Premium
+				Sealed Tube, so total is 4.2 Premium Sealed Tube. (if lucky you can get
+				4-4)
+			</div>
+			<div>so you need to do 7 runs to get 29 Premium Sealed Tube</div>
+			<div>7 runs = 420 energy = 1.75 days = 4x a week</div>
+			<div>each run also gives 20 premium tuner</div>
+			<div>need 50 tuner to 5x tune 1 echo</div>
+			<div>so you need to do 2.5 runs to get 50 tuner</div>
+			<div>2.5 runs = 150 energy = 0,625 days = 11,2x a week</div>
+			<div>
+				so in a week you can level at least 4 echoes and max tune 11 echoes
+			</div>
+			<div>
+				(obviously this doesn't count tube and tuner you get from dailies, bp,
+				event, etc)
+			</div>
+			<div>#####################################</div>
+			<div>#####################################</div>
+			<div>#####################################</div>
+			<div>#####################################</div>
+			<div>#####################################</div>
+			<div>#####################################</div>
+			<div>#####################################</div>
+			<div>#####################################</div>
+			<div>#####################################</div>
+			<div>#####################################</div>
+			<div>#####################################</div>
+			<div>#####################################</div>
+			<div>#####################################</div>
+			<div>#####################################</div>
 		</div>
 	</section>
 
 	<section>
-		<div>todo</div>
-		<div>
-			implement done button: update current_ and decrease inventory items
-		</div>
-		<div>
-			home page to show stamina, current planned char/weap, and needed materials
-		</div>
-		<div>show synthesizable materials</div>
-		<div>done button also affects decreases synthesizable materials</div>
+		<div>TODO</div>
+		<div>improv:</div>
+		<div>delete char/weap from localstorage if done</div>
+		<div>add link from home, click char/weap, then open the detail page</div>
+		<div>improve exp to be calc-ed similar to tiered materials</div>
+		<div>new feature:</div>
 		<div>add backup and restore local storage data</div>
 		<div>sort by in-game: sort by rarity, category? a-z?</div>
 		<div>add custom text notes</div>
 		<div>try google drive client api if it's actually possible</div>
 		<div>idk</div>
+		<div>learn css/tailwind mayb</div>
 	</section>
 </template>
 
