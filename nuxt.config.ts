e224c678d8817e392d592288f44aad17599c8483
boxofzero <elnino.ftorres09@gmail.com>
2024-07-30 22:54:20 +0700

    remove the rest of vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
@@ -10,9 +8,7 @@ export default defineNuxtConfig({
 		'@nuxt/ui',
 		'@vueuse/nuxt',
 		'@pinia/nuxt',
-		'vuetify-nuxt-module',
 		'nuxt-lodash',
-		'vite-plugin-vuetify',
 		'@nuxtjs/color-mode',
 	],
 	compatibilityDate: '2024-07-13',
