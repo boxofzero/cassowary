399ced0e2042b040a490f03d9ce1a124f5c47408
boxofzero <elnino.ftorres09@gmail.com>
2024-08-01 12:54:44 +0700

    fix stamina button

--- components/Stamina.vue
+++ components/Stamina.vue
@@ -3,9 +3,7 @@
 		<h1 class="text-4xl font-extrabold">Stamina Tracker</h1>
 	</section>
 
-	<section
-		class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 sm:gap-16 mb-10"
-	>
+	<section class="flex flex-wrap gap-5">
 		<div>
 			<div class="font-bold">Track Your Stamina</div>
 			<div class="text-2xl font-extrabold mb-2">
@@ -17,7 +15,7 @@
 			</div>
 		</div>
 		<div class="">
-			<div class="font-bold">Your Stamina will be</div>
+			<div class="font-bold">Your Stamina will be:</div>
 			<div class="text-sm mb-2 columns-1">
 				<!-- {{ moment(staminaStore.fullStaminaAt).format('YYYY-MM-DD HH:mm:ss') }} -->
 				<div v-for="(item, index) in calculateFutureStaminaAt()" :key="index">
@@ -25,9 +23,9 @@
 				</div>
 			</div>
 		</div>
-		<div>
+		<div class="">
 			<div class="font-bold">Adjust Stamina</div>
-			<div class="grid grid-cols-3 gap-1">
+			<div class="grid grid-cols-3 gap-2">
 				<div
 					class="h-8 w-10 flex items-center justify-center"
 					v-for="(item, index) in ['-1', '-40', '-60', '+1', '+40', '+60']"
