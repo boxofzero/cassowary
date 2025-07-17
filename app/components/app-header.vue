58e1fbee8835c93a9e6daf583cc0e7fcc12267fc
boxofzero <elnino.ftorres09@gmail.com>
2025-07-17 18:34:59 +0700

    fix: fix app-header navigation menu

--- app/components/app-header.vue
+++ app/components/app-header.vue
@@ -1,14 +1,51 @@
 <template>
-	<header class="flex justify-between items-center my-5">
-		<UNavigationMenu
-			orientation="horizontal"
-			:items="links"
-			class="border-gray-800 border-b data-[orientation=vertical]:w-48"
-		/>
+	<header class="my-5">
+		<div class="flex justify-between">
+			<!-- Desktop: Horizontal Menu -->
+			<UNavigationMenu
+				v-if="isMediumScreen"
+				orientation="horizontal"
+				:items="links"
+			/>
+
+			<!-- Mobile Popover Navigation -->
+			<UDropdownMenu
+				v-else
+				:items="links"
+				:ui="{
+					content: 'w-48',
+				}"
+			>
+				<UButton icon="i-lucide-menu" color="neutral" variant="outline" />
+			</UDropdownMenu>
+			<div class="flex items-center">
+				<UBadge
+					:avatar="{
+						src: useRuntimeConfig().app.baseURL + 'favicon.ico',
+					}"
+					:trailing-icon="icon"
+					@click="isDark = !isDark"
+					variant="outline"
+					size="lg"
+					color="neutral"
+				>
+					Cassowary (Pre-alpha version)
+				</UBadge>
+			</div>
+		</div>
 	</header>
 </template>
 
 <script setup>
+import { breakpointsTailwind, useBreakpoints } from '@vueuse/core';
+
+// Responsive utility: show dropdown on small screens
+const breakpoints = useBreakpoints(breakpointsTailwind);
+// adjust breakpoint if needed
+const isBigScreen = breakpoints.greater('xl');
+const isMediumScreen = breakpoints.greater('md');
+const isSmallScreen = breakpoints.smaller('lg');
+
 // dark mode
 const colorMode = useColorMode();
 const isDark = computed({
@@ -34,51 +71,35 @@ watch(isDark, (newValue) => {
 
 // link config
 const links = ref([
-	[
-		{
-			label: 'Home',
-			icon: 'material-symbols:house-with-shield',
-			to: '/',
-		},
-		{
-			label: 'Inventory',
-			icon: 'material-symbols:checked-bag',
-			to: '/inventoryItems',
-		},
-		{
-			label: 'Characters',
-			icon: 'material-symbols:emoji-people-rounded',
-			to: '/characters',
-		},
-		{
-			label: 'Weapons',
-			icon: 'material-symbols:sword-rose',
-			to: '/weapons',
-		},
-		{
-			label: 'Settings',
-			icon: 'material-symbols:settings',
-			to: '/settings',
-		},
-		{
-			label: 'About',
-			icon: 'material-symbols:help',
-			to: '/about',
-		},
-	],
-	[
-		{
-			label: 'Cassowary (Pre-alpha version)',
-			avatar: {
-				src: useRuntimeConfig().app.baseURL + 'favicon.ico',
-			},
-		},
-		{
-			icon: icon.value,
-			onClick: () => {
-				isDark.value = !isDark.value;
-			},
-		},
-	],
+	{
+		label: 'Home',
+		icon: 'material-symbols:house-with-shield',
+		to: '/',
+	},
+	{
+		label: 'Inventory',
+		icon: 'material-symbols:checked-bag',
+		to: '/inventoryItems',
+	},
+	{
+		label: 'Characters',
+		icon: 'material-symbols:emoji-people-rounded',
+		to: '/characters',
+	},
+	{
+		label: 'Weapons',
+		icon: 'material-symbols:sword-rose',
+		to: '/weapons',
+	},
+	{
+		label: 'Settings',
+		icon: 'material-symbols:settings',
+		to: '/settings',
+	},
+	{
+		label: 'About',
+		icon: 'material-symbols:help',
+		to: '/about',
+	},
 ]);
 </script>
